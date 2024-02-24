2b0096bebbf15abbedcdb86bdd27634050f04028
ttiimmothy <dinosauli2006@gmail.com>
2024-02-23 23:32:17 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# General Code
+[![ci](https://github.com/ttiimmothy/general-code/actions/workflows/ci.yml/badge.svg)](https://github.com/ttiimmothy/general-code/actions/workflows/ci.yml)
+
+General Code is for storing the codes which I use.
+
+## License
+
+General Code is licensed under [GNU General Public License v3.0](LICENSE).
---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+USERNAME="ttiimmothy"
+TOKEN=process.env.TOKEN
+
+REPO_URL="https://api.github.com/user/repos?per_page=100"
+REPOS=$(curl -s -H "Authorization: Bearer ${TOKEN}" ${REPO_URL} | grep -o '"ssh_url": "git@github.com:ttiimmothy[^"]*"' | awk -F'"' '{print $4}')
+
+echo "{\"repository_url\":{" > repository.json
+index=1
+for repo in ${REPOS}; do
+  repo_name=$(basename ${repo})
+  echo "\"${index}\": [\"${repo_name}\"]," >> repository.json
+  ((index++))
+done
+echo "}}" >> repository.json
+
+# for repo in ${REPOS}; do
+#   repo_name=$(basename ${repo} .git)
+#   repo_name_with_underscore=${repo_name//-/_}
+#   git clone "${repo}" "${repo_name_with_underscore}"
+# done
+
+# for repo in ${REPOS}; do
+#   repo_name=$(basename ${repo} .git)
+#   repo_name_with_underscore=${repo_name//-/_}
+#   echo "${repo_name_with_underscore}"
+# done
