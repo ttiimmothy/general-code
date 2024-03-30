49117545ca0e579de109d4b3eac2800c22bb8575
ttiimmothy <dinosauli2006@gmail.com>
2024-03-30 15:37:32 -0400

    feat: update the path of repository.json in scripts

--- scriptForGithubOrganization.sh
+++ scriptForGithubOrganization.sh
@@ -5,14 +5,16 @@ TOKEN=""
 REPO_URL="https://api.github.com/user/repos?per_page=100"
 REPOS=$(curl -s -H "Authorization: Bearer ${TOKEN}" ${REPO_URL} | grep -o '"ssh_url": "git@github.com:openrice-canada[^"]*"' | awk -F'"' '{print $4}')
 
-echo "{\"repository_url\":{" > repository.json
+OPENRICE_ORGANIZATION_PATH="$HOME/developer/github_repository/openrice_canada_organization"
+
+echo "{\"repository_url\":{" > $OPENRICE_ORGANIZATION_PATH/repository.json
 index=1
 for repo in ${REPOS}; do
   repo_name=$(basename ${repo})
-  echo "\"${index}\": [\"${repo_name}\"]," >> repository.json
+  echo "\"${index}\": [\"${repo_name}\"]," >> $OPENRICE_ORGANIZATION_PATH/repository.json
   ((index++))
 done
-echo "}}" >> repository.json
+echo "}}" >> $OPENRICE_ORGANIZATION_PATH/repository.json
 
 # for repo in ${REPOS}; do
 #   repo_name=$(basename ${repo} .git)
